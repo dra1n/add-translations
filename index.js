82cf4d78affc9811a7957936261ac5f4a5eb54bc
Andrey Bakuta <anb@anahoret.com>
2016-06-17 18:34:06 +0300

    Use vars

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-const csv = require('fast-csv');
-const xml2js = require('xml2js');
-const lens = require('lorgnette').lens;
+var fs = require('fs');
+var csv = require('fast-csv');
+var xml2js = require('xml2js');
+var lens = require('lorgnette').lens;
 
-const sax = require('sax');
+var sax = require('sax');
 
 sax.ENTITIES['lt'] = '&lt;'
 sax.ENTITIES['gt'] = '&gt;'
 
 function withDictionary(file, callback) {
-  const dict = {};
-  const stream = fs.createReadStream(file);
+  var dict = {};
+  var stream = fs.createReadStream(file);
 
-  const csvStream = csv()
+  var csvStream = csv()
   .on("data", function(data){
     dict[data[0]] = data[1];
   })
@@ -25,8 +25,8 @@ function withDictionary(file, callback) {
 
 function withXiff(file, callback) {
   try {
-    const fileData = fs.readFileSync(file, 'utf8');
-    const parser = new xml2js.Parser()
+    var fileData = fs.readFileSync(file, 'utf8');
+    var parser = new xml2js.Parser()
     parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
       //callback();
       callback(result);
@@ -40,12 +40,12 @@ function withXiff(file, callback) {
 module.exports = function(csvFilePath, xliffFilePath, callback) {
   withDictionary(csvFilePath, function(dict) {
     withXiff(xliffFilePath, function(xliff) {
-      const builder = new xml2js.Builder({xmldec: { 'version': '1.0', 'encoding': 'UTF-8', 'standalone': false}} );
+      var builder = new xml2js.Builder({xmldec: { 'version': '1.0', 'encoding': 'UTF-8', 'standalone': false}} );
 
       xliff['xliff']['file'].forEach(function(r) {
         r['body'].forEach(function(r) {
           r['trans-unit'].forEach(function(r) {
-            const value = dict[r['source'][0]];
+            var value = dict[r['source'][0]];
 
             if (value) {
               r['target'] = value;
